4ad54520ade594dd8128eed1b5caafa310bc2742
Matt Redmon <mredmon333@gmail.com>
2017-05-10 21:58:31 -0400

    adding initial work Enumberable documentation

---
+++ enumerable_01.rb
@@ -0,0 +1,7 @@
+# Enumerable Module
+
+# the Enumerable mixin provides collection classes with traversal, searching and sort methods
+# the class must provide a method each, which yields successive members of the collection
+# if Enumerable#max, #min, or #sort is used, the objects in the collection must also
+# implement a meaningful <=> operator, as these methods rely on an ordering between members of collection
+
